b6dad5cd296259b4bd3eba730f2b0bea03869487
Amesika <am3sika@gmail.com>
2022-03-10 03:59:32 +0100

    Mise en place de log fonctionnelles et techniques

--- application/quizz-api/src/main/java/com/amesika/quizzapi/controllers/WelcomeController.java
+++ application/quizz-api/src/main/java/com/amesika/quizzapi/controllers/WelcomeController.java
@@ -1,13 +1,18 @@
 package com.amesika.quizzapi.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
+    private static Logger logger = LogManager.getLogger(WelcomeController.class);
     
     @GetMapping
     public String getIndex() {
+        logger.info("Welcome - Quizz-Api");
         return "Welcome - Quizz-Api";
     }
 }
