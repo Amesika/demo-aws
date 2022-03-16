fcf52bc687c6ee37388cd5ff0ce994eb38568c67
Amesika <am3sika@gmail.com>
2022-03-16 03:36:33 +0100

    closes #2;YOUR COMMIT MESSAGE

--- application/quizz-api/src/main/java/com/amesika/quizzapi/entities/Question.java
+++ application/quizz-api/src/main/java/com/amesika/quizzapi/entities/Question.java
@@ -16,7 +16,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter; 
+import lombok.Getter;  
 import lombok.NoArgsConstructor;
 import lombok.Setter;  
 import lombok.ToString;
