b3e6462432d48a12e0569e0955c746022a03392b
Amesika <am3sika@gmail.com>
2022-03-16 03:23:15 +0100

    closed #2

--- application/quizz-api/src/main/java/com/amesika/quizzapi/entities/Question.java
+++ application/quizz-api/src/main/java/com/amesika/quizzapi/entities/Question.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Setter; 
 import lombok.ToString;
 
 @Entity
